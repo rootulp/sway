83f0090cb047abcdaa690b0d4f99f55122b64c3c
mitchmindtree <mitchell.nordine@fuel.sh>
2022-04-18 14:31:17 +1000

    Fix incorrect temporary namespace in impl trait type check

--- sway-core/src/semantic_analysis/ast_node/impl_trait.rs
+++ sway-core/src/semantic_analysis/ast_node/impl_trait.rs
@@ -328,7 +328,7 @@ fn type_check_trait_implementation(
 
     // this name space is temporary! It is used only so that the below methods
     // can reference functions from the interface
-    let mut local_namespace = Namespace::default();
+    let mut local_namespace = namespace.clone();
     local_namespace.insert_trait_implementation(
         CallPath {
             prefixes: vec![],
